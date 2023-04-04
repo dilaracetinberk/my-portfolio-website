ae4a0e74759f536a3344029a6c6fef4685989aa8
kodluyoruz <dilaracetinberk@gmail.com>
2023-04-04 09:15:00 +0300

    fix menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,19 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
 
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
   return (
     <nav className='navbar shadow-md'>
        <button
           type="button"
           onClick={() => setOpen(!open)}
-          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+          className="text-3xl absolute right-8 top-6 cursor-pointer sm:hidden"
         >
            < GiHamburgerMenu name={open ? 'close' : 'menu'}/>
         </button>
       <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? 'top-30 ' : 'top-[-490px]'
+            open ? 'top-30' : 'top-[-490px]'
           }`}>
         <li className="md:ml-8 lg:text-xl text-[16px] md:my-3 my-7">
           <Link to='/' className='navbar-a'>
